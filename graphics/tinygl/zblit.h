80e4ffe60e355244e45bd11ae400d228b25a52e5
Stefano Musumeci <subr3v@gmail.com>
2014-07-04 15:12:08 +0200

    TINYGL: refined blitting draft (squash this)

--- graphics/tinygl/zblit.h
+++ graphics/tinygl/zblit.h
@@ -6,20 +6,16 @@
 namespace TinyGL {
 
 
-	void tglUploadBlitTexture(int* textureHandle, Graphics::PixelBuffer &buffer, int colorKey);
+	void tglUploadBlitTexture(int* textureHandle, int width, int height, Graphics::PixelBuffer &buffer, int colorKey);
 	void tglDisposeBlitTexture(int textureHandle);
 
-	template <bool disableBlending = false, bool disableColoring = false, bool disableTransform = false>
-	void tglBlit(int blitTextureHandle, int dstX, int dstY, int width, int height, int srcX, int srcY, int srcWidth, int srcHeight, float rotation, float rTint, float gTint, float bTint, float aTint);
-
-	// Disables blending, coloring and transform.
+	void tglBlit(int blitTextureHandle, int dstX, int dstY, int width, int height, int srcX, int srcY, int srcWidth, int srcHeight, float rotation = 0, float rTint = 1.0f, float gTint = 1.0f, float bTint = 1.0f, float aTint = 1.0f);
+	
+	// Disables blending explicitly.
+	void tglBlitNoBlend(int blitTextureHandle, int dstX, int dstY, int width, int height, int srcX, int srcY, int srcWidth, int srcHeight, float rotation, float rTint, float gTint, float bTint, float aTint);
+	
+	// Disables blending, transforms and tinting.
 	void tglBlitFast(int blitTextureHandle, int x, int y, int width, int height);
-}
-
-template <bool disableBlending = false, bool disableColoring = false, bool disableTransform = false>
-void TinyGL<disableBlending>::tglBlit(int blitTextureHandle, int dstX, int dstY, int width, int height, int srcX, int srcY, int srcWidth, int srcHeight, float rotation, float rTint, float gTint, float bTint, float aTint)
-{
-
 }
 
 #endif
