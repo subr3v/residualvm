c1fc35a3ab03b69363823309bf4b54399d8d3e00
Joni Vähämäki <jonivah@gmail.com>
2014-08-05 23:11:50 +0300

    EMI: Do not disable writing to color buffer if an actor's sort order is >= 100.
    Fixes invisible Guybrush in the set unc.

--- engines/grim/gfx_tinygl.cpp
+++ engines/grim/gfx_tinygl.cpp
@@ -568,11 +568,6 @@ void GfxTinyGL::startActorDraw(const Actor *actor) {
 		tglScalef(scale, scale, scale);
 		tglMultMatrixf(quat.toMatrix().getData());
 	}
-
-	if (!_currentShadowArray && actor->getSortOrder() >= 100) {
-		tglColorMask(TGL_FALSE, TGL_FALSE, TGL_FALSE, TGL_FALSE);
-		tglDepthMask(TGL_TRUE);
-	}
 }
 
 void GfxTinyGL::finishActorDraw() {
